e44eb24453c508dde11a2473d26a38a188fc9155
nathandrussell <nathandrussell98@gmail.com>
2018-02-08 10:20:26 -0500

    Added all files

---
+++ Hacker/Client.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Hacker
+{
+        enum Encryption
+        {
+            NONE,
+            ROT13
+        }
+    class Client
+    {
+        public NetworkStream stream { get; set; }
+        public string nickname = "Other user";
+
+        private TcpClient tcp = null;
+        private Stream outputStream = null;
+        private int port { get; set; }
+        private string ip { get; set; }
+        
+
+        public Encryption encryptionType = Encryption.NONE;
+
+        //Initializes Client with the default values.
+        public Client(bool isHost = false, string inputAddress = "127.0.0.1", int inputPort = 6789)
+        {
+
+            outputStream = Console.OpenStandardOutput();
+            ip = inputAddress;
+            port = inputPort;
+            if (isHost)
+            {
+                host();
+            }
+            else
+            {
+                client();
+            }
+
+            stream = tcp.GetStream();
+            outputToStream(getCurrentTime() + "Connected to " + ip + ":" + port);
+
+
+        }
+        //Starts Connection as host
+        private void host()
+        {
+            TcpListener host = new TcpListener(IPAddress.Parse(ip), port);
+            host.Start();
+
+            outputToStream(getCurrentTime() + "Listening on " + ip + ":" + port);
+            tcp = host.AcceptTcpClient();
+
+            outputToStream(getCurrentTime() + " " + DateTime.Now.ToShortTimeString() + ": User Connected");
+        }
+        //Starts connection as client
+        private void client()
+        {
+            tcp = new TcpClient("localhost", port);
+        }
+
+        string getCurrentTime(string dateFormat = "MM/dd/yyyy h:mm:ss ")
+        {
+            return DateTime.Now.ToString(dateFormat);
+        }
+
+        static Message rot13(Message m)
+        {
+            if (m.type == "message")
+            {
+                string firstHalfCharacters  = "abcdefghijklmABCDEFGHIJKLM";
+                string secondHalfCharacters = "nopqrstuvwxyzNOPQRSTUVWXYZ";
+
+                char[] firstHalf = firstHalfCharacters.ToCharArray();
+                char[] secondHalf = secondHalfCharacters.ToCharArray();
+
+                string cipherMessage = "";
+
+                foreach (char c in m.message)
+                {
+
+                    if (firstHalfCharacters.Contains(c))
+                    {
+                        int value = firstHalfCharacters.IndexOf(c);
+                        cipherMessage += secondHalf[value];
+                    }
+                    else if (secondHalfCharacters.Contains(c))
+                    {
+                        int value = secondHalfCharacters.IndexOf(c);
+                        cipherMessage += firstHalf[value];
+                    }
+                    else
+                    {
+                        cipherMessage += c;
+                    }
+
+                }
+
+                m.message = cipherMessage;
+            }
+            return m;
+        }
+
+        public Message receiveData()
+        {
+            byte[] receieveData = new byte[100000];
+            int bytes = stream.Read(receieveData, 0, receieveData.Length);
+
+            Message msg = JsonConvert.DeserializeObject<Message>(Encoding.ASCII.GetString(receieveData, 0, bytes));
+
+            if (encryptionType == Encryption.ROT13)
+            {
+                msg = rot13(msg);
+            }
+
+            if (msg.type == "nickChange")
+            {
+                nickname = msg.message;
+            }
+            return msg;
+        }
+
+        public void sendMessage(string messageInput)
+        {
+            Message tempMessage;
+
+            byte[] sendData;
+
+            tempMessage = scanInput(messageInput);
+
+            if (encryptionType == Encryption.ROT13)
+            {
+                tempMessage = rot13(tempMessage);
+            }
+
+            sendData = System.Text.Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(tempMessage));
+            stream.Write(sendData, 0, sendData.Length);
+        }
+
+        Message scanInput(string i)
+        {
+            Message m = new Message();
+
+            if (i.StartsWith("/nick"))
+            {
+                m.type = "nickChange";
+                m.message = i.Remove(0, 5).Trim();
+            }
+            else if (i.StartsWith("/crypt"))
+            {
+                string encryptionToUse = i.Remove(0, 6).Trim();
+                
+                if (encryptionToUse.ToUpper() == "ROT13")
+                {
+                    outputToStream(getCurrentTime() + "ROT13 cipher enabled");
+                    encryptionType = Encryption.ROT13;
+                }
+                else if (encryptionToUse.ToUpper() == "OFF")
+                {
+                    outputToStream(getCurrentTime() + "rot13 cipher off");
+                    encryptionType = Encryption.NONE;
+                }
+                m.type = "nickChange";
+                m.message = nickname;
+                
+            }
+            else
+            {
+                m.type = "message";
+                m.message = i;
+            }
+
+            return m;
+
+        }
+        void outputToStream(string message)
+        {
+            byte[] dataMessage = Encoding.ASCII.GetBytes(message);
+            int length = dataMessage.Length;
+
+            outputStream.Write(dataMessage, 0, length);
+            outputStream.Write(Encoding.ASCII.GetBytes("\n"), 0, Encoding.ASCII.GetBytes("\n").Length);
+        }
+    }
+}
---
+++ Hacker/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Hacker
+{
+    //Simple struct for a message type
+    struct Message
+    {
+        public string type;
+        public string message;
+    }
+
+    class Program
+    {
+
+        static Client client = null; //Global Client declaration
+
+        static void Main(string[] args)
+        {
+            //Argument Handling
+            bool isHost = false;
+            
+            if (args.Length > 0)
+            {
+                if (args[0] == "-host")
+                {
+                    isHost = true;
+                }
+            }
+            //Runs startup
+            startup(isHost);
+
+            Console.ReadKey(); //Keeps window open at end of program to view final results
+
+        }
+
+        static void startup(bool isHost)
+        {
+            Console.Title = "Hacker Chat";
+
+
+            client = new Client(isHost); //Initializes the client
+            //starts Threads for Sending and Recieving data.
+            if (client != null)
+            {
+                Thread th = new Thread(new ThreadStart(receiveThread));
+                Thread th2 = new Thread(new ThreadStart(sendThread));
+
+                th.Start();
+                th2.Start();
+
+            }
+        }
+        //Recieves data and performs appropriate tasks.
+        static void receiveThread()
+        {
+            while (true)
+            {
+                Message received = client.receiveData();
+
+                if (received.type == "message")
+                {
+                    Console.WriteLine(client.nickname + ": " + received.message);
+                }
+            }
+        }
+        //Takes user input to send to other user
+        static void sendThread()
+        {
+            string read = "";
+            bool reset = false;
+            
+            
+            while (true)
+            {
+                var a = Console.ReadKey();
+                if (a.Key == ConsoleKey.Enter && !reset)
+                {
+                    client.sendMessage(read);
+                    reset = true;
+                    read = "";
+                    Console.WriteLine();
+                }
+                else if (char.IsLetterOrDigit(a.KeyChar) || char.IsPunctuation(a.KeyChar))
+                {
+                    read += a.KeyChar;
+                    reset = false;
+                }
+            }
+        }
+        //Changes message type based on User Input and ensures proper tags
+
+
+    }
+}
